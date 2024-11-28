be4e12b197da1e5877a1846472361522e7a18cb3
”Askar” <askarmullahanov7@gmail.com>
2024-11-28 09:35:38 +0300

    added get position player

--- src/client/main.client.ts
+++
@@ -1,8 +0,0 @@
-const ReplicatedStorage = game.GetService("ReplicatedStorage");
-const IconClickedEvent = ReplicatedStorage.WaitForChild("IconClickedEvent") as RemoteEvent;
-
-const Icon = script.Parent as ClickDetector;
-
-Icon.MouseClick.Connect(() => {
-	IconClickedEvent.FireServer();
-});
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,19 +1,20 @@
 const PlayerService = game.GetService("Players");
-const ReplicatedStorage = game.GetService("ReplicatedStorage");
 
 PlayerService.PlayerAdded.Connect((player) => {
 	print(`Игрок ${player.Name} подключился к серверу!`);
+
+	player.CharacterAdded.Connect((character) => {
+		const humanoidRootPart = character.FindFirstChild("HumanoidRootPart");
+		if (humanoidRootPart?.IsA("BasePart")) {
+			const position = humanoidRootPart.Position;
+			print(`Позиция игрока ${player.Name}: X=${position.X}, Y=${position.Y}, Z=${position.Z}`);
+		} else {
+			print(`HumanoidRootPart не найден у игрока ${player.Name}`);
+		}
+	});
 });
 
 PlayerService.PlayerRemoving.Connect((player) => {
 	print(`Игрок ${player.Name} отключился от сервера!`);
 });
 
-// Создание RemoteEvent с помощью конструктора класса
-const IconClickedEvent = new Instance("RemoteEvent");
-IconClickedEvent.Name = "IconClickedEvent";
-IconClickedEvent.Parent = ReplicatedStorage;
-
-IconClickedEvent.OnServerEvent.Connect((player: Player) => {
-	print(`Игрок ${player.Name} нажал на иконку!`);
-});
