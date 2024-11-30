ae7b863853f741a97dd21b846e5a0ae61b477a35
”Askar” <askarmullahanov7@gmail.com>
2024-11-30 03:54:40 +0300

    connect charm

--- src/client/controllers/click.ts
+++ src/client/controllers/click.ts
@@ -1,18 +1,64 @@
 import { Controller, OnStart } from "@flamework/core";
 import { UserInputService } from "@rbxts/services";
 import { ClientEvents } from "shared/events";
+import { atom } from "@rbxts/charm";
+import { Players } from "@rbxts/services";
+import CharmSync from "@rbxts/charm-sync";
+import { DataStoreService } from "@rbxts/services";
 
 @Controller({})
 export class Click implements OnStart {
-	public onStart() {
-		UserInputService.InputBegan.Connect((input) => {
-			if (input.UserInputType !== Enum.UserInputType.MouseButton1) return;
+	private clickAtom = atom<number>(0);
+	private dataStore = DataStoreService.GetDataStore("PlayerClicks");
 
-			ClientEvents.click.fire();
+	public async onStart() {
+	 	const player = Players.LocalPlayer;
+
+		const key = `Player_${player.UserId}`;
+		const [savedClicks, _] = await this.dataStore.GetAsync(key);
+		if (savedClicks !== undefined) {
+			this.clickAtom(savedClicks as number);
+		}
+ 
+		const syncer = CharmSync.client({
+			atoms: { clicks: this.clickAtom },
 		});
 
 		ClientEvents.updateClicks.connect((clicks) => {
-			print(`your clicks is ${clicks}`)
-		})
+			this.clickAtom.(clicks);
+			print(`Your clicks is ${clicks}`);
+		});
+
+		UserInputService.InputBegan.Connect((input) => {
+			if (input.UserInputType !== Enum.UserInputType.MouseButton1) return;
+
+			const currentClicks = this.clickAtom.get();
+			this.clickAtom.set(currentClicks + 1);
+
+			ClientEvents.click.fire();
+		});
+		player.AncestryChanged.Connect((_, parent) => {
+			if (parent === undefined) {
+				this.saveClicks();
+			}
+		});
+		}
+ 
+		private async saveClicks() {
+			const player = Players.LocalPlayer;
+			const key = `Player_${Players.LocalPlayer.UserId}`;
+			const clicks = this.clickAtom();
+			await this.dataStore.SetAsync(key, clicks);
+		}
+	
 	}
-}
+  
+	// UserInputService.InputBegan.Connect((input) => {
+		// 	if (input.UserInputType !== Enum.UserInputType.MouseButton1) return;
+
+		// 	ClientEvents.click.fire();
+		// });
+
+		// ClientEvents.updateClicks.connect((clicks) => {
+		// 	print(`your clicks is ${clicks}`)
+		// })
--- src/server/services/player-service.ts
+++ src/server/services/player-service.ts
@@ -1,20 +1,42 @@
 import { OnStart, Service } from "@flamework/core";
-import { atom } from "@rbxts/charm";
+import Charm, { atom } from "@rbxts/charm";
+import CharmSync from "@rbxts/charm-sync";
 import { Players } from "@rbxts/services";
-import { ServerEvents } from "shared/events";
+import { ClientEvents, ServerEvents } from "shared/events";
 
 @Service()
 export class PlayerService implements OnStart {
 	private clicks: Map<Player, number> = new Map();
-
+	private clicksAtom = atom<number>(0);
+	private syncer = CharmSync.server({
+		atoms: { clicks: this.clicksAtom },
+	});
+ 
 	public onStart() {
-		ServerEvents.click.connect((player) => {
+        ServerEvents.click.connect((player) => {
 			const clicks = this.clicks.get(player) ?? 0;
-
 			this.clicks.set(player, clicks + 1);
 			ServerEvents.updateClicks.fire(player, clicks + 1);
+        });
+		Players.PlayerAdded.Connect((player) => {
+			const clicksAtom = atom<number>(0);
+			this.clicks.set(player, clicksAtom());
+
+			const syncer = CharmSync.server({
+				atoms: { clicks: clicksAtom },
+			});
+			this.syncer.hydrate(player);
+
+			ServerEvents.click.connect((clickedPlayer) => {
+				if (clickedPlayer === player) {
+					const currentClicks = clicksAtom();
+					clicksAtom(currentClicks + 1);
+                    ServerEvents.updateClicks.fire(player, currentClicks + 1);
+				}
+			});
 		});
+        Players.PlayerRemoving.Connect((player) => {
+            this.clicks.delete(player);
+        })
 	}
 }
-
-
